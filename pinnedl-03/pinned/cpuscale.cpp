3434ef2b195d9b812a8b76d3f9527d78289cef59
hxc3427 <harshdeepchawla@gmail.com>
2016-10-17 14:27:58 -0400

    Update cpuscale.cpp

--- pinnedl-03/pinned/cpuscale.cpp
+++ pinnedl-03/pinned/cpuscale.cpp
@@ -1,3 +1,10 @@
+//
+//  CPU.cpp
+//  hpalab5
+//
+//  Created by Harshdeep Singh Chawla on 10/4/16.
+//  Copyright © 2016 Harshdeep Singh Chawla. All rights reserved.
+//
 #include "common.h"
 
 void scaleVectorCPU( float* a, float* c, float scaleFactor, int size ){
